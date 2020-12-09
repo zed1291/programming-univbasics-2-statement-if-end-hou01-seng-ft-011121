2e983fe196fbee7d858d7dd8346ad0b9cd6bbea2
zed <zed@kolk.me>
2020-12-09 19:58:52 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,6 +1,3 @@
-run_code_inside = true
-puts "Code before if...end"
-if run_code_inside
-  puts "code inside"
-end
-puts "Code after if...end"
+current_time = Time.now
+current_time.to_s
+p current_time
